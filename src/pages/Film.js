79283bd7135e823bcbc50be055732baadd2a4b89
leigh966 <leigh966@hotmail.com>
2022-02-24 17:48:45 +0000

    the film page now fetches the title of the film with id given where the url is like .../film/{id}. This required large restructuring of the Film.js so that a Film() function still catches the id through the React hook. Then all state related items are handled by the class filmPage. This code should be generalized to prevent code repetition before attempting to implement the actor page

--- src/pages/Film.js
+++ src/pages/Film.js
@@ -1,8 +1,38 @@
+import React from "react";
 import { BrowserRouter as Router, useParams } from "react-router-dom";
 
-function Film() {
-  let { id } = useParams();
-  return <h1>Film:{id}</h1>;
+class FilmPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filmInfo: null,
+    };
+    console.log(this.props.id);
+    var id = this.props.id;
+    fetch(`http://18.170.34.114:8080/home/get_film?id=${id}`)
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        this.setState({
+          filmInfo: json,
+        });
+      });
+  }
+
+  render() {
+    if (this.state.filmInfo) {
+      console.log("filmInfo.title: " + this.state.filmInfo[0].title);
+      return <h1>{this.state.filmInfo[0].title}</h1>;
+    } else {
+      return <h1>Loading...</h1>;
+    }
+  }
+}
+
+export function Film() {
+  const { id } = useParams();
+  console.log({ id });
+  return <FilmPage id={id[0]} />;
 }
 
 export default Film;
