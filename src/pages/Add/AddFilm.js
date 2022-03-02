ae7f8ca37cdf67d908006a324eeb1ad570df4658
leigh966 <leigh966@hotmail.com>
2022-03-02 17:33:50 +0000

    overridden saveFilm() in AddFilm to send a post request rather than a PUT request.
    Since title and rating are the only tracked variables for updating at the moment, some hard coded values have been put in for now

--- src/pages/Add/AddFilm.js
+++ src/pages/Add/AddFilm.js
@@ -1,8 +1,29 @@
 import { FilmPage } from "../RecordPage/Film";
+import { getRoot } from "../../API_config";
 
 class AddFilmPage extends FilmPage {
   constructor(props) {
     super(props);
+  }
+
+  saveFilm() {
+    console.log(this.state.title);
+    const title = this.state.title;
+    const rating = this.state.rating;
+    const releaseYear = 2006;
+    const languageId = 1;
+    const length = 86;
+    const description = "test object";
+
+    fetch(
+      `http://${getRoot()}/home/add_film?` +
+        `title=${title}&rating=${rating}` +
+        `&release_year=${releaseYear}&language_id=${languageId}` +
+        `&rental_duration=0&rental_rate=0&` +
+        `length=${length}&description=${description}` +
+        `&replacement_cost=0`,
+      { method: "POST" }
+    );
   }
 }
 
