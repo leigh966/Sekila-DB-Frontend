31a3ccccd6b1fead7b5813e537d295f7a403e82f
leigh966 <leigh966@hotmail.com>
2022-03-02 21:27:54 +0000

    Idea code created for LangaugeDropdown component

---
+++ src/Dropdowns/LangaugeDropdown.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
+import { getRoot } from "../API_config";
+
+export class LanguageDropdown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.sendRequest();
+  }
+
+  onResponse(json) {
+    this.setState({
+      languages: json,
+    });
+  }
+
+  sendRequest() {
+    fetch(`http://${getRoot()}/get_languages`)
+      .then((response) => response.json())
+      .then((json) => {
+        this.onResponse(json);
+      })
+      .catch((e) =>
+        this.setState({
+          errorMessage: e.message,
+        })
+      );
+  }
+
+  getOptions() {
+    let options = [];
+    this.state.languages.forEach((language) => {
+      options.push(langauge.name);
+    });
+    return options;
+  }
+
+  render() {
+    if (this.state.errorMessage) return <h4>{this.state.errorMessage}</h4>;
+    if (!languages) return <h4>Loading...</h4>;
+
+    const options = this.getOptions();
+    return (
+      <Dropdown
+        options={options}
+        onChange={(event) => {
+          this.props.handler(event.value);
+        }}
+        value={this.props.current}
+        placeholder="Select an option"
+      />
+    );
+  }
+}
