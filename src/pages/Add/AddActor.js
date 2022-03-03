35f75b0974bc1ee0bd6cfda7c803841246d3c8f1
leigh966 <leigh966@hotmail.com>
2022-03-03 10:14:38 +0000

    Added saveActor override to post an actor when save is pressed

--- src/pages/Add/AddActor.js
+++ src/pages/Add/AddActor.js
@@ -1,8 +1,26 @@
 import { ActorPage } from "../RecordPage/Actor";
+import { getRoot } from "../../API_config";
 
 class AddActorPage extends ActorPage {
   constructor(props) {
     super(props);
+  }
+  saveActor() {
+    console.log(this.state.first_name);
+    console.log(this.state.last_name);
+    const first_name = this.state.first_name;
+    const last_name = this.state.last_name;
+    fetch(
+      `http://${getRoot()}/home/add_actor?first_name=${first_name}&last_name=${last_name}`,
+      {
+        method: "POST",
+      }
+    )
+      .then((response) => response.text())
+      .then((text) => {
+        console.log(text);
+        window.alert(text);
+      });
   }
 }
 
